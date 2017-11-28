84033bf469bb43fc5c2bc5c2d7d957a6b4f791a6
Vivek <vvkkumar06@gmail.com>
2017-11-28 20:26:50 +0530

    lesson -3

--- app/src/main/java/com/macrowiz/vivek/swoosh/MainActivity.kt
+++ app/src/main/java/com/macrowiz/vivek/swoosh/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.macrowiz.vivek.swoosh
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+            getStartedBtn.setOnClickListener{
+                   val  LeagueIntent =  Intent(this, LeagueActivity::class.java)
+                    startActivity(LeagueIntent)
+            }
     }
 }
